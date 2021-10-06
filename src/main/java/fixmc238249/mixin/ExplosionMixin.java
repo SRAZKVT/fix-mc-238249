06be4671ffb70d8964ce3d41052d370afac05c60
Sarah-Mathey <sarah.mathey66666@gmail.com>
2021-10-06 12:49:12 +0200

    better fix, now caches 5x5x5 around the origin position

--- src/main/java/fixmc238249/mixin/ExplosionMixin.java
+++ src/main/java/fixmc238249/mixin/ExplosionMixin.java
@@ -24,9 +24,8 @@ public class ExplosionMixin {
     @Shadow @Final private float power;
 
     private final BlockPos posOrigin = new BlockPos(x, y, z);
-    private Float blastResAtOrigin = null;
-    private boolean isOriginPosSet = false;
-    private boolean isOriginPosAir = false;
+    private Float[][][] blastResNearOrigin = new Float[5][5][5];
+    private Boolean[][][] isPosNearOriginAir = new Boolean[5][5][5];
 
     @Inject(
             method = "collectBlocksAndDamageEntities",
@@ -39,22 +38,25 @@ public class ExplosionMixin {
         while (rayStrength > 0.0F) {
             BlockPos blockPos = new BlockPos(x, y, z);
 
+            int xindex = posOrigin.getX() - blockPos.getX() + 2;
+            int yindex = posOrigin.getY() - blockPos.getY() + 2;
+            int zindex = posOrigin.getZ() - blockPos.getZ() + 2;
+
             float blastResOfBlock;
             boolean isBlockAir;
 
-            if ((blockPos.getX() != posOrigin.getX() && blockPos.getY() != posOrigin.getY() && blockPos.getZ() != posOrigin.getZ()) || !isOriginPosSet) {
+            if (!isWithinBounds(xindex, yindex, zindex) || !isPositionCached(xindex, yindex, zindex)) {
                 BlockState blockState = this.world.getBlockState(blockPos);
                 FluidState fluidState = this.world.getFluidState(blockPos);
                 blastResOfBlock = (Math.max(blockState.getBlock().getBlastResistance(), fluidState.getBlastResistance()) + 0.3F) * 0.3F;
                 isBlockAir = blockState.isAir() && fluidState.isEmpty();
-                if (!isOriginPosSet) {
-                    this.blastResAtOrigin = blastResOfBlock;
-                    isOriginPosSet = true;
-                    isOriginPosAir = isBlockAir;
+                if (isWithinBounds(xindex, yindex, zindex)) {
+                    this.blastResNearOrigin[xindex][yindex][zindex] = blastResOfBlock;
+                    isPosNearOriginAir[xindex][yindex][zindex] = isBlockAir;
                 }
             } else {
-                blastResOfBlock = blastResAtOrigin;
-                isBlockAir = isOriginPosAir;
+                blastResOfBlock = blastResNearOrigin[xindex][yindex][zindex];
+                isBlockAir = isPosNearOriginAir[xindex][yindex][zindex];
             }
             if (!isBlockAir) {
                 rayStrength -= blastResOfBlock;
@@ -81,5 +83,13 @@ public class ExplosionMixin {
     )
     private float removeVanillaForLoop() {
         return -this.power;
+    }
+
+    private boolean isWithinBounds(int xindex, int yindex, int zindex) {
+        return xindex >= 0 && xindex < 4 && yindex >= 0 && yindex < 4 && zindex >= 0 && zindex < 4;
+    }
+
+    private boolean isPositionCached(int xindex, int yindex, int zindex) {
+        return blastResNearOrigin[xindex][yindex][zindex] != null;
     }
 }
